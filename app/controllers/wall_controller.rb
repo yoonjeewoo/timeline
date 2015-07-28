b545e5e0dd3776bdbdc7facf8c39dfae9a9748be
jeewooyoon <yoonjeewoo@gmail.com>
2015-07-28 16:40:33 +0900

    타임라인 글 수정 후 저장 및 화면 전환 기능 추가하기

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -15,6 +15,17 @@ class WallController < ApplicationController
   def edit
     @post_edit = Post.find(params[:id])
   end
+  def edit_complete
+    p = Post.find(params[:id])
+    p.name = params[:writer_edit]
+    p.content = params[:content_edit]
+    if p.save
+      redirect_to "/wall/posts"
+    else
+      flash[:alert] = p.errors[:content][0]
+      redirect_to :back
+    end
+  end
   def posts
     @posts = Post.all
   end
