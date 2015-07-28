2c8e8c9981909aeb116a30415818c41abf8f0df9
jeewooyoon <yoonjeewoo@gmail.com>
2015-07-28 16:56:31 +0900

    타임라인 글 삭제 및 화면 전환 기능 추가하기

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -29,6 +29,12 @@ class WallController < ApplicationController
   def delete
     @post_delete = Post.find(params[:id])
   end
+  def delete_complete
+    p = Post.find(params[:id])
+    p.destroy
+
+    redirect_to "/wall/posts"
+  end
   def posts
     @posts = Post.all
   end
